bd0cc9784b6c322cddf633eb62b2375c82f3d7c4
sushant871998 <sushanthope@gmail.com>
2020-09-21 14:43:58 +0530

    First Commit

---
+++ project-tool/project-tool/src/main/java/com/bottomline/projecttool/ProjectToolApplication.java
@@ -0,0 +1,13 @@
+package com.bottomline.projecttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToolApplication.class, args);
+	}
+
+}
---
+++ project-tool/project-tool/src/test/java/com/bottomline/projecttool/ProjectToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottomline.projecttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
